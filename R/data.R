4793bc024e58d6418c547d87269d55653698c89e
UncleCamsWaterPlans <cameron.roberts@des.qld.gov.au>
2022-02-10 15:01:41 +1000

    Fix syntax errors in rmarkdown

--- R/data.R
+++ R/data.R
@@ -1,5 +1,4 @@
 #' List of available 'cities' with weather data through OpenWeather.org
-#'
 #' @format a data frame containing 209579 obs. of  6 variables
 #' \describe{
 #'   \item{id}{unique identifier}
@@ -14,114 +13,108 @@
 
 
 #' Reference table for all of WQI's location sources with LAB data
-#'
 #' Provides Node ID's for available discrete analytes
-#'
 #' @format a data frame containing 134 obs. of  56 variables
 #' \describe{
-#'  \item{_id}
-#'  \item{GSnum}
-#'  \item{name}
-#'  \item{TextFileSource}
-#'  \item{WQ ANALYSIS NO}
-#'  \item{Site Name}
-#'  \item{SITE NO}
-#'  \item{Atrazine}
-#'  \item{Ametryn}
-#'  \item{Ammonium nitrogen}
-#'  \item{RT Level}
-#'  \item{Bromacil}
-#'  \item{Chlorpyrifos}
-#'  \item{Conductivity}
-#'  \item{Diss Kjeldahl nitrogen}
-#'  \item{Diss Kjeldahl phosphor}
-#'  \item{Diss organic nitrogen}
-#'  \item{Diss organic phosphor}
-#'  \item{Diuron}
-#'  \item{Filt react phosphorus}
-#'  \item{Fipronil}
-#'  \item{Fluometuron}
-#'  \item{Fluroxypyr}
-#'  \item{Glyphosate}
-#'  \item{Haloxyfop acid}
-#'  \item{Hexazinone}
-#'  \item{Imazapic}
-#'  \item{Imidacloprid}
-#'  \item{Isoxaflutole}
-#'  \item{MCPA}
-#'  \item{Metolachlor}
-#'  \item{Metribuzin}
-#'  \item{Metsulfuron-methyl}
-#'  \item{Oxidised nitrogen}
-#'  \item{Particulate Nitrogen}
-#'  \item{Particulate phosphorus}
-#'  \item{Pendimethalin}
-#'  \item{Prometryn}
-#'  \item{Simazine}
-#'  \item{Tebuthiuron}
-#'  \item{Terbuthylazine}
-#'  \item{Terbutryn}
-#'  \item{Total organic carbon}
-#'  \item{Triclopyr}
-#'  \item{TSS}
-#'  \item{Ttl Kjeldahl nitrogen}
-#'  \item{Ttl nitrogen}
-#'  \item{Ttl phosphorus}
-#'  \item{Turbidity}
-#'  \item{Chlorothalonil}
-#'  \item{Diazinon}
-#'  \item{Propiconazole}
-#'  \item{RT TSSeq}
-#'  \item{RT Conductivity}
-#'  \item{RT Turbidity}
-#'  \item{WQI Site Name}
-#'
+#'   \item{_id}{_id}
+#'   \item{GSnum}{GSnum}
+#'   \item{name}{name}
+#'   \item{TextFileSource}{TextFileSource}
+#'   \item{WQ ANALYSIS NO}{WQ ANALYSIS NO}
+#'   \item{Site Name}{Site Name}
+#'   \item{SITE NO}{SITE NO}
+#'   \item{Atrazine}{Atrazine}
+#'   \item{Ametryn}{Ametryn}
+#'   \item{Ammonium nitrogen}{Ammonium nitrogen}
+#'   \item{RT Level}{RT Level}
+#'   \item{Bromacil}{Bromacil}
+#'   \item{Chlorpyrifos}{Chlorpyrifos}
+#'   \item{Conductivity}{Conductivity}
+#'   \item{Diss Kjeldahl nitrogen}{Diss Kjeldahl nitrogen}
+#'   \item{Diss Kjeldahl phosphor}{Diss Kjeldahl phosphor}
+#'   \item{Diss organic nitrogen}{Diss organic nitrogen}
+#'   \item{Diss organic phosphor}{Diss organic phosphor}
+#'   \item{Diuron}{Diuron}
+#'   \item{Filt react phosphorus}{Filt react phosphorus}
+#'   \item{Fipronil}{Fipronil}
+#'   \item{Fluometuron}{Fluometuron}
+#'   \item{Fluroxypyr}{Fluroxypyr}
+#'   \item{Glyphosate}{Glyphosate}
+#'   \item{Haloxyfop acid}{Haloxyfop acid}
+#'   \item{Hexazinone}{Hexazinone}
+#'   \item{Imazapic}{Imazapic}
+#'   \item{Imidacloprid}{Imidacloprid}
+#'   \item{Isoxaflutole}{Isoxaflutole}
+#'   \item{MCPA}{MCPA}
+#'   \item{Metolachlor}{Metolachlor}
+#'   \item{Metribuzin}{Metribuzin}
+#'   \item{Metsulfuron-methyl}{Metsulfuron-methyl}
+#'   \item{Oxidised nitrogen}{Oxidised nitrogen}
+#'   \item{Particulate Nitrogen}{Particulate Nitrogen}
+#'   \item{Particulate phosphorus}{Particulate phosphorus}
+#'   \item{Pendimethalin}{Pendimethalin}
+#'   \item{Prometryn}{Prometryn}
+#'   \item{Simazine}{Simazine}
+#'   \item{Tebuthiuron}{Tebuthiuron}
+#'   \item{Terbuthylazine}{Terbuthylazine}
+#'   \item{Terbutryn}{Terbutryn}
+#'   \item{Total organic carbon}{Total organic carbon}
+#'   \item{Triclopyr}{Triclopyr}
+#'   \item{TSS}{TSS}
+#'   \item{Ttl Kjeldahl nitrogen}{Ttl Kjeldahl nitrogen}
+#'   \item{Ttl nitrogen}{Ttl nitrogen}
+#'   \item{Ttl phosphorus}{Ttl phosphorus}
+#'   \item{Turbidity}{Turbidity}
+#'   \item{Chlorothalonil}{Chlorothalonil}
+#'   \item{Diazinon}{Diazinon}
+#'   \item{Propiconazole}{Propiconazole}
+#'   \item{RT TSSeq}{RT TSSeq}
+#'   \item{RT Conductivity}{RT Conductivity}
+#'   \item{RT Turbidity}{RT Turbidity}
+#'   \item{WQI Site Name}{WQI Site Name}
 #' }
 #' @source \url{https://wqi.eagle.io/}
 "labRef"
 
 
 #' Reference table for all of WQI's location sources with Logger data
-#'
 #' Provides Node ID's for available real time parameters
-#'
 #' @format a data frame containing 134 obs. of  56 variables
 #' \describe{
-#'   \item{_id}
-#'   \item{GSnum}
-#'   \item{name}
-#'   \item{loggerID}
-#'   \item{Main - Level}
-#'   \item{OPUSResults - OPUS1000 N NO3}
-#'   \item{OPUSResults - OPUS1036 Abs210}
-#'   \item{Nitrate - NICO RefA}
-#'   \item{OPUSResults - OPUS1042 Abs254}
-#'   \item{Nitrate - NICO RefB}
-#'   \item{OPUSResults - OPUS1034 Abs360}
-#'   \item{Nitrate - NICO RefC}
-#'   \item{Nitrate - NICO RefD}
-#'   \item{OPUSResults - OPUS1060 SQI}
-#'   \item{OPUSResults - OPUS1016 TSSeq}
-#'   \item{Public - RSSI}
-#'   \item{Resets - WDE}
-#'   \item{Public - OPUS Serial}
-#'   \item{Public - SondePresent}
-#'   \item{Public - NitratePresent}
-#'   \item{Public - ADCPPresent}
-#'   \item{Main - LoggerBattV Min}
-#'   \item{Main - PestFrV Min}
-#'   \item{Main - PumpBattV Min}
-#'   \item{Public - SamplerOnOff}
-#'   \item{Public - SamplerState}
-#'   \item{FridgeStatus - NFridgeStatus}
-#'   \item{FridgeStatus - PFridgeStatus}
-#'   \item{Public - SampleNo}
-#'   \item{Sonde - SondeTemp}
-#'   \item{Sonde - Conductivity}
-#'   \item{Sonde - Turbidity}
-#'   \item{Sonde - DO-mgL}
-#'   \item{Sonde - fDOM-QSU}
-#'
+#'   \item{_id}{_id}
+#'   \item{GSnum}{GSnum}
+#'   \item{name}{name}
+#'   \item{loggerID}{loggerID}
+#'   \item{Main - Level}{Main - Level}
+#'   \item{OPUSResults - OPUS1000 N NO3}{OPUSResults - OPUS1000 N NO3}
+#'   \item{OPUSResults - OPUS1036 Abs210}{OPUSResults - OPUS1036 Abs210}
+#'   \item{Nitrate - NICO RefA}{Nitrate - NICO RefA}
+#'   \item{OPUSResults - OPUS1042 Abs254}{OPUSResults - OPUS1042 Abs254}
+#'   \item{Nitrate - NICO RefB}{Nitrate - NICO RefB}
+#'   \item{OPUSResults - OPUS1034 Abs360}{OPUSResults - OPUS1034 Abs360}
+#'   \item{Nitrate - NICO RefC}{Nitrate - NICO RefC}
+#'   \item{Nitrate - NICO RefD}{Nitrate - NICO RefD}
+#'   \item{OPUSResults - OPUS1060 SQI}{OPUSResults - OPUS1060 SQI}
+#'   \item{OPUSResults - OPUS1016 TSSeq}{OPUSResults - OPUS1016 TSSeq}
+#'   \item{Public - RSSI}{Public - RSSI}
+#'   \item{Resets - WDE}{Resets - WDE}
+#'   \item{Public - OPUS Serial}{Public - OPUS Serial}
+#'   \item{Public - SondePresent}{Public - SondePresent}
+#'   \item{Public - NitratePresent}{Public - NitratePresent}
+#'   \item{Public - ADCPPresent}{Public - ADCPPresent}
+#'   \item{Main - LoggerBattV Min}{Main - LoggerBattV Min}
+#'   \item{Main - PestFrV Min}{Main - PestFrV Min}
+#'   \item{Main - PumpBattV Min}{Main - PumpBattV Min}
+#'   \item{Public - SamplerOnOff}{Public - SamplerOnOff}
+#'   \item{Public - SamplerState}{Public - SamplerState}
+#'   \item{FridgeStatus - NFridgeStatus}{FridgeStatus - NFridgeStatus}
+#'   \item{FridgeStatus - PFridgeStatus}{FridgeStatus - PFridgeStatus}
+#'   \item{Public - SampleNo}{Public - SampleNo}
+#'   \item{Sonde - SondeTemp}{Sonde - SondeTemp}
+#'   \item{Sonde - Conductivity}{Sonde - Conductivity}
+#'   \item{Sonde - Turbidity}{Sonde - Turbidity}
+#'   \item{Sonde - DO-mgL}{Sonde - DO-mgL}
+#'   \item{Sonde - fDOM-QSU}{Sonde - fDOM-QSU}
 #' }
 #' @source \url{https://wqi.eagle.io/}
 "loggerRef"
